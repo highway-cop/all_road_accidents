2a6d9ec53d21466d1c81316c4890e63d1873209c
listanegra <vitormac3@hotmail.com>
2021-08-09 08:42:15 -0300

    Google Maps widget state & search

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,19 +1,109 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+import 'RestService.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final auth = FirebaseAuth.instance;
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  Completer<GoogleMapController> _controller = Completer();
+
+  final cidadeController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    if (auth.currentUser == null) {
+      throw ('Usuário não autenticado');
+    }
+
+    User user = auth.currentUser as User;
+
+    String photoURL = user.photoURL ??
+        "https://cdn.iconscout.com/icon/free/png-256/account-avatar-profile-human-man-user-30448.png";
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Acidentes'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.near_me_outlined),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.filter_alt_outlined),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  content: Stack(
+                    children: [
+                      TextField(
+                        controller: cidadeController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Cidade',
+                        ),
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    TextButton(
+                      child: const Text('Buscar'),
+                      onPressed: () async {
+                        setState(() {
+                          markers = {};
+                        });
+
+                        final results =
+                            await RestService.getByCity(cidadeController.text);
+
+                        final map = results.asMap().map((key, value) {
+                          MarkerId markerId = MarkerId(key.toString());
+
+                          Marker marker = Marker(
+                              markerId: markerId, position: value.location);
+
+                          return MapEntry(markerId, marker);
+                        });
+
+                        setState(() {
+                          markers.addAll(map);
+                        });
+
+                        Navigator.of(context).pop();
+                        cidadeController.clear();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
+            UserAccountsDrawerHeader(
+              accountName: Text(user.displayName ?? ""),
+              accountEmail: Text(user.email ?? ""),
+              currentAccountPicture: CircleAvatar(
+                radius: 30.0,
+                backgroundImage: NetworkImage(photoURL),
+                backgroundColor: Colors.white,
+              ),
+            ),
             ListTile(
               title: const Text('Item 1'),
               onTap: () {
@@ -29,32 +119,17 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: _MapSample(),
-    );
-  }
-}
-
-class _MapSample extends StatefulWidget {
-  @override
-  _MapSampleState createState() => _MapSampleState();
-}
-
-class _MapSampleState extends State<_MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition: _kGooglePlex,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: Set.of(markers.values),
+        initialCameraPosition: CameraPosition(
+          zoom: 8,
+          target: LatLng(-25, -50),
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
